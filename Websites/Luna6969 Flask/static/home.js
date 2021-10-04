62edf5a38d73f254df056b0853fb09a76534048a
LUNA761 <lagdenzach09@gmail.com>
2021-10-04 16:00:18 +0100

    deletes

--- Websites/Luna6969 Flask/static/home.js
+++
@@ -1,9 +0,0 @@
-function triggernav() {
-document.getElementById("nav").style.width = "250px";
-document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
-
-function closenav() {
-document.getElementById("nav").style.width = "0";
-document.body.style.backgroundColor = "#222";
-}
